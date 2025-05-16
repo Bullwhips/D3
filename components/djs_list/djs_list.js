30b7156c8694c6bfb9e4620b871823e342008f7a
Hampus833 <hampuse266@gmail.com>
2025-05-16 13:19:55 +0200

    djlist container

---
+++ components/djs_list/djs_list.js
@@ -0,0 +1,14 @@
+function renderDjList(container) {
+    let nav = document.createElement("nav");
+    for (let dj of DJs) {
+        let div = document.createElement("div");
+        let p = document.createElement("p");
+
+        div.setAttribute("id", dj.id);
+        p.textContent = dj.name;
+
+        div.appendChild(p);
+        nav.appendChild(div);
+    }
+    container.appendChild("nav");
+}
