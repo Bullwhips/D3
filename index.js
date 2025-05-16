af495b1adc1a7dfcf9c1746d78a95fff7cad5cc0
Bullwhips <epicj4444@gmail.com>
2025-05-16 13:11:31 +0200

    smoll

---
+++ index.js
@@ -0,0 +1,11 @@
+const wrapper = document.createElement("main")
+wrapper.id = "wrapper"
+document.querySelector("body").append(wrapper)
+
+function startApp(wrapper) {
+renderHeader(wrapper)
+renderDjList(wrapper)
+    
+}
+
+startApp(wrapper)
