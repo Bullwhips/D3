d97cc6ffd197b0ae0935155396f51ed11b848c97
Hampus833 <hampuse266@gmail.com>
2025-05-16 13:47:50 +0200

    added css

--- components/djs_list/djs_list.js
+++ components/djs_list/djs_list.js
@@ -1,5 +1,14 @@
 function renderDjList(container) {
     let nav = document.createElement("nav");
+    let allDjs = document.createElement("div");
+    allDjs.setAttribute("id", "allDjs");
+
+    let allDjsP = document.createElement("p");
+    allDjsP.textContent = "DJ:s";
+
+    allDjs.appendChild(allDjsP);
+
+    nav.appendChild(allDjs);
     for (let dj of DJs) {
         let div = document.createElement("div");
         let p = document.createElement("p");
